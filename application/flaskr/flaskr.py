c7f839f0b00672401ed7dfb3ece1d18b8f39a38d
Toby Wilkins <tobwilk@gmail.com>
2016-08-30 14:45:05 +0000

    sample app dev

--- application/flaskr/flaskr.py
+++ application/flaskr/flaskr.py
@@ -27,9 +27,19 @@ def connect_db():
   return rv
 
 
+def get_db():
+  """Opens a new database connection if there is none yet for the
+  current application context.
+  """
+  if not hasattr(g, 'sqlite_db'):
+    g.sqlite_db = connect_db()
+  return g.sqlite_db
 
-
-
+@app.teardown_appcontext
+def close_db(error):
+  """Closes the database again at the end of the request."""
+  if hasattr(g, 'sqlite_db'):
+    g.sqlite_db.close()
 
 
 
