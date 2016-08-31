434ad3b07f69468aeee36c128ee777aabc53c93b
Toby Wilkins <tobwilk@gmail.com>
2016-08-31 12:01:21 +0000

    modified so that input vpc and subnet are automatically provided

--- application/launch-stack.sh
+++ application/launch-stack.sh
@@ -1,1 +1,18 @@
-aws cloudformation create-stack --stack-name devsecops-flaskr --template-body file://application.json --parameters ParameterKey=KeyName,ParameterValue=toby_NDRResearchKey ParameterKey=VPC,ParameterValue=vpc-ecbc3f88 ParameterKey=Subnet,ParameterValue=subnet-8d9f01fb
+#!/bin/bash
+
+# Get ouput variable from the environment stack
+json=`aws cloudformation describe-stacks --stack-name devsecops-environment`
+
+# Pull out the VPC
+vpcid=`echo $json | jq -r '.Stacks[0].Outputs[0].OutputValue'`
+echo $vpcid
+
+# Pull out the subnet
+subnetid=`echo $json | jq -r '.Stacks[0].Outputs[1].OutputValue'`
+echo $subnetid
+
+# Launch the application stack with these values
+aws cloudformation create-stack --stack-name devsecops-flaskr --template-body file://application.json --parameters ParameterKey=KeyName,ParameterValue=toby_NDRResearchKey ParameterKey=VPC,ParameterValue=${vpcid} ParameterKey=Subnet,ParameterValue=${subnetid}
+
+
+
