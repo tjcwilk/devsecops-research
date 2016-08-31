9a66f3d97dd4b195d778c9b3f7cc93783aa0baf3
Toby Wilkins <tobwilk@gmail.com>
2016-08-31 12:11:47 +0000

    commented out sqlite init so we can do it else where

--- application/flaskr/setup.sh
+++ application/flaskr/setup.sh
@@ -2,4 +2,4 @@ sudo apt-get update -y
 sudo apt-get install -y python-pip
 sudo pip install Flask
 sudo apt-get install -y sqlite3
-sqlite3 /tmp/flaskr.db < schema.sql
+#sqlite3 /tmp/flaskr.db < schema.sql
